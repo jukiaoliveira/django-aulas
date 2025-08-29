5701b7f6154b24fe229d69e28ae176cb963b161a
Julia Oliveira <jurafaoliveira@icloud.com>
2025-08-28 22:07:55 -0300

    utilizando com outra tabela

--- my_ads/dvdrentalapp/urls.py
+++ my_ads/dvdrentalapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('customer/', views.customer, name='mycustomer'),
+    path('detalhes/<int:id>', views.detalhes, name='myDetalhe'),
+    path('pagamentos/<int:id>', views.pagamentos, name='myPayment'),
     path('category/', views.category, name='category'),
 ]
--- my_ads/dvdrentalapp/views.py
+++ my_ads/dvdrentalapp/views.py
@@ -4,7 +4,8 @@
 
 from django.http import HttpResponse
 from django.template import loader
-from .models import Customer, Category
+from .models import Customer, Category, Rental, Payment
+from django.shortcuts import get_object_or_404
 
 def customer(request):
     mycustomers = Customer.objects.all().values()
@@ -13,6 +14,28 @@ def customer(request):
         'mycustomer': mycustomers,
 }
 
+    return HttpResponse(template.render(context, request))
+
+def detalhes(request, id):
+    myDetalhes = Rental.objects.filter(customer_id=id)
+    customer = get_object_or_404(Customer, pk=id)
+
+    template = loader.get_template('detalhes.html')
+    context = {
+        'myDetalhe' : myDetalhes,
+        'customer_name' : f"{customer.first_name} {customer.last_name}",
+    }
+    return HttpResponse(template.render(context, request))
+
+def pagamentos(request, id):
+    myPayment = Payment.objects.filter(customer_id=id)
+    customer = get_object_or_404(Customer, pk=id)
+
+    template = loader.get_template('pagamentos.html')
+    context = {
+        'myPayment' : myPayment,
+        'customer_name' : f"{customer.first_name} {customer.last_name}",
+    }
     return HttpResponse(template.render(context, request))
 
 def category(request):
