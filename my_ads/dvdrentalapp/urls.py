38764a976928d60a59246ecbc3ab339c4eaff2aa
Julia Oliveira <jurafaoliveira@icloud.com>
2025-09-03 22:03:11 -0300

    atividade utilizando POST

--- my_ads/dvdrentalapp/urls.py
+++ my_ads/dvdrentalapp/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('customer/', views.customer, name='mycustomer'),
+    path('address/<int:id>', views.address, name='myaddress'),
+    path('edit_address/<int:address_id>', views.edit_address, name='edit_address'),
+    path('customer1/', views.customer1, name='customer1'),
     path('detalhes/<int:id>', views.detalhes, name='myDetalhe'),
+    path('edit_customer/<int:customer_id>', views.edit_customer, name='edit_customer'),
     path('pagamentos/<int:id>', views.pagamentos, name='myPayment'),
     path('category/', views.category, name='category'),
+    path('category1/', views.category1, name='category1'),
+    path('edit_category/<int:category_id>/', views.edit_category, name='edit_category'),
 ]
--- my_ads/dvdrentalapp/views.py
+++ my_ads/dvdrentalapp/views.py
@@ -4,8 +4,9 @@
 
 from django.http import HttpResponse
 from django.template import loader
-from .models import Customer, Category, Rental, Payment
-from django.shortcuts import get_object_or_404
+from .models import Customer, Category, Rental, Payment, Address
+from django.shortcuts import get_object_or_404, render, redirect 
+from datetime import datetime
 
 def customer(request):
     mycustomers = Customer.objects.all().values()
@@ -15,6 +16,26 @@ def customer(request):
 }
 
     return HttpResponse(template.render(context, request))
+
+def customer1(request):
+    mycustomers = Customer.objects.all().values()
+    template = loader.get_template('list_customers.html')
+    context = {
+        'customer1': mycustomers,
+}
+
+    return HttpResponse(template.render(context, request))
+
+def edit_customer(request, customer_id):
+    customer = get_object_or_404(Customer, pk=customer_id)
+    if request.method == "POST":
+        customer.first_name = request.POST.get('first_name')
+        customer.last_name = request.POST.get('last_name')
+        customer.email = request.POST.get('email')
+
+        customer.save()
+        return redirect('/customer')
+    return render(request, 'edit_customer.html', {'customer': customer})
 
 def detalhes(request, id):
     myDetalhes = Rental.objects.filter(customer_id=id)
@@ -26,6 +47,28 @@ def detalhes(request, id):
         'customer_name' : f"{customer.first_name} {customer.last_name}",
     }
     return HttpResponse(template.render(context, request))
+
+def address(request, id):
+    myAddress = Address.objects.filter(customer__id=id)
+    customer = get_object_or_404(Customer, pk=id)
+
+    template = loader.get_template('all_address.html')
+    context = {
+        'myAddress': myAddress,
+        'customer_name': f"{customer.first_name} {customer.last_name}",
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def edit_address(request, address_id):
+    address = get_object_or_404(Address, pk=address_id)
+    if request.method == "POST":
+        address.address2 = request.POST.get('address2')
+        address.postal_code = request.POST.get('postal_code')
+        address.save()
+        return redirect(f'/address/{address_id}')
+    return render(request, 'edit_address.html', {'address': address})
+
 
 def pagamentos(request, id):
     myPayment = Payment.objects.filter(customer_id=id)
@@ -46,3 +89,21 @@ def category(request):
 }
 
     return HttpResponse(template.render(context, request))
+
+def category1(request):
+    mycategorys = Category.objects.all().values()
+    template = loader.get_template('category.html')
+    context = {
+        'category1': mycategorys,
+    }
+    return HttpResponse(template.render(context, request))
+ 
+def edit_category(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    if request.method == "POST":
+        category.name = request.POST.get('name')
+        category.last_update = datetime.now()
+        category.save()
+        return redirect('/category1')
+    return render(request, 'edit_category.html', {'category' : category})
+ 
