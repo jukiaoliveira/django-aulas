1c5b13d965e44cd72a501dd856a88eeafc43ffad
Julia Oliveira <jurafaoliveira@icloud.com>
2025-09-10 22:19:06 -0300

    queryset e queryset reference

--- my_ads/dvdrentalapp/views.py
+++ my_ads/dvdrentalapp/views.py
@@ -4,8 +4,8 @@
 
 from django.http import HttpResponse
 from django.template import loader
-from .models import Customer, Category, Rental, Payment, Address, Film, Language
-from django.shortcuts import get_object_or_404, render, redirect 
+from .models import Customer, Category, Rental, Payment, Address, Film, Language, Country
+from django.shortcuts import get_object_or_404, render, redirect
 from datetime import datetime
 from django.utils import timezone
 
@@ -108,9 +108,9 @@ def add_category(request):
         category1.save()
 
         return redirect('/category1')
-    
+
     return render(request, 'add_category.html')
- 
+
 def edit_category(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
     if request.method == "POST":
@@ -119,7 +119,7 @@ def edit_category(request, category_id):
         category.save()
         return redirect('/category1')
     return render(request, 'edit_category.html', {'category' : category})
- 
+
 def list_filme(request):
     films = Film.objects.all()
     return render(request, 'list_filme.html', {'films':films})
@@ -129,10 +129,150 @@ def add_film(request):
         name = request.POST.get('name')
 
         if name:
-            film = Film(title=name, language_id=1, rental_duration=3, rental_rate=4.99, replacement_cost=19.99, last_update=timezone.now()) 
+            film = Film(title=name, language_id=1, rental_duration=3, rental_rate=4.99, replacement_cost=19.99, last_update=timezone.now())
             film.save()
             return redirect('/film')
         else:
             return render(request, 'add_film.html', {'error': 'O nome do filme é obrigatório.'})
 
     return render(request, 'add_film.html')
+
+# def listacustomer(request):
+#     mycustomers = Customer.objects.all().values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name__contains='Karen').values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(last_name__icontains='mill').values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name__endswith='s').values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name__iendswith='s').values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name__exact='Phyllis').values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name__iexact='phyllis').values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name__in=['Phyllis', 'Dennis', 'Nicholas']).values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(customer_id__gt=500).values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(customer_id__gte=500).values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(customer_id__lt=15).values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(customer_id__lte=15).values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# # def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name = 'Maria', customer_id=7).values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# def listacustomer(request):
+#     mycustomers = Customer.objects.filter(first_name = 'Maria').values() | Customer.objects.filter(customer_id=8).values()
+#     template = loader.get_template('customers.html')
+#     context = {
+#         'listcustomer':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# def listacustomer1(request):
+#     search_name = request.GET.get('search_name', '')
+
+#     if search_name:
+#         mycustomers = Customer.objects.filter(first_name__contains=search_name).values()
+#     else:
+#         mycustomers = Customer.objects.all().values()
+
+#     template = loader.get_template('customers1.html')
+#     context = {
+#         'listcustomer1':mycustomers,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+def listacountry(request):
+    search_name = request.GET.get('search_name', '')
+
+    if search_name:
+        mycountry = Country.objects.filter(country__contains=search_name).values() | Country.objects.filter(country_id__contains=search_name).values()
+    else:
+        mycountry = Country.objects.all().values()
+
+    template = loader.get_template('country.html')
+    context = {
+        'listcountry':mycountry,
+    }
+    return HttpResponse(template.render(context, request))
